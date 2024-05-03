37040c351bad6b485e118bf4d69b9ea277c1a830
antoine30000 <antoine.beaurin@eemi.com>
2024-05-03 11:45:39 +0200

    wip: search pokemon by name

--- pages/PokemonList.vue
+++ pages/PokemonList.vue
@@ -3,30 +3,41 @@ export default {
   data() {
     return {
       loading: true,
-      pokemonList: []
+      pokemonList: [],
+      searchQuery: ''
     };
   },
   async mounted() {
     try {
-      const response = await $fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+      const response = await $fetch('https://pokeapi.co/api/v2/pokemon?limit=151'); // Fetching first 151 Pokémon
       this.pokemonList = response.results;
       this.loading = false;
     } catch (error) {
-      console.error('Error while loading pokemons :', error);
+      console.error('Erreur lors de la récupération des Pokémon :', error);
     }
-    console.log(this.pokemonList)
+  },
+  computed: {
+    filteredPokemonList() {
+      return this.pokemonList.filter(pokemon => {
+        return pokemon.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+      });
+    }
+  },
+  methods: {
+    goToPokemonDetails(pokemonName) {
+      this.$router.push(`/pokemon/${pokemonName}`);
+    }
   }
 };
-
-
 </script>
 
 <template>
   <div>
     <h1>Liste des Pokémon</h1>
-    <div v-if="loading">Loading...</div>
+    <input type="text" v-model="searchQuery" placeholder="Rechercher un Pokémon..." />
+    <div v-if="loading">Chargement...</div>
     <div v-else>
-      <div v-for="pokemon in pokemonList" :key="pokemon.id">
+      <div v-for="pokemon in filteredPokemonList" :key="pokemon.id" @click="goToPokemonDetails(pokemon.name)">
         <div>
           <h2>{{ pokemon.name }}</h2>
           <ul>
@@ -38,8 +49,3 @@ export default {
     </div>
   </div>
 </template>
-
-  
-
-  
-  
