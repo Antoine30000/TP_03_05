043d6a7af123633af0742071edac745b8485e292
antoine30000 <antoine.beaurin@eemi.com>
2024-05-03 11:39:41 +0200

    feat: list of 10 pokemon

--- pages/PokemonList.vue
+++ pages/PokemonList.vue
@@ -1,29 +1,44 @@
- <script>
-async function getPokemon(){
-    const pokemon = await $fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
-}
-console.log(getPokemon())
-  </script>
+<script>
+export default {
+  data() {
+    return {
+      loading: true,
+      pokemonList: []
+    };
+  },
+  async mounted() {
+    try {
+      const response = await $fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+      this.pokemonList = response.results;
+      this.loading = false;
+    } catch (error) {
+      console.error('Error while loading pokemons :', error);
+    }
+    console.log(this.pokemonList)
+  }
+};
 
-  <template>
-    <div>
-      <h1>List of Pokémon Cards</h1>
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <div v-for="pokemon in pokemonList" :key="pokemon.id">
-          <div>
-            <h2>{{ pokemon.name }}</h2>
-            <ul>
-              <li v-for="(ability, index) in pokemon.abilities" :key="index">{{ ability.name }}</li>
-            </ul>
-            <img :src="pokemon.images.small" alt="pokemon" />
-          </div>
+
+</script>
+
+<template>
+  <div>
+    <h1>Liste des Pokémon</h1>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <div v-for="pokemon in pokemonList" :key="pokemon.id">
+        <div>
+          <h2>{{ pokemon.name }}</h2>
+          <ul>
+            <li v-for="(ability, index) in pokemon.abilities" :key="index">{{ ability.name }}</li>
+          </ul>
+          <img :src="`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`" alt="pokemon" />
         </div>
       </div>
     </div>
-  </template>
-  
- 
+  </div>
+</template>
+
   
 
   
